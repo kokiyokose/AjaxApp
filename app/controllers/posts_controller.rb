047ef7b6003be074335a42466c8ed5763890b0df
kokiyokose <koki.yokose@di-v.co.jp>
2020-07-03 16:36:12 +0900

    既読機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,23 @@
 class PostsController < ApplicationController
-def index  
-  @posts = Post.all  # すべてのレコードを@postsに代入
-　end
+  def index
+    @posts = Post.all.order(id: "DESC")
+  end
 
- def new  
- end
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+    redirect_to action: :index
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      post.update(checked: false)
+    else
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
 end
